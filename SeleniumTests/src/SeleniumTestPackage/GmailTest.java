7e23d53826748779bb74ae3ef8f654addfae7acc
User42 <User42@DESKTOP-H240ICE.Home>
2018-03-04 00:07:34 +0000

    Test classpath doesn't re-appear on github now

--- SeleniumTests/src/SeleniumTestPackage/GmailTest.java
+++ SeleniumTests/src/SeleniumTestPackage/GmailTest.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GmailTest {
 	
 	////
+	////
 	public static void main(String[] args) {
 		
 		File file = new File("C:/chromedriver.exe");
